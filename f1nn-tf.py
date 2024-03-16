a344911b16311e464cd86bb0db1adbd2c9e59930
Stephan Schauberger <stephan.schauberger@gmail.com>
2024-03-16 19:29:30 +0100

    feat(tf) finish example

--- f1nn-tf.py
+++ f1nn-tf.py
@@ -8,6 +8,14 @@ BATCH_SIZE = 2
 EPOCHS = 20
 
 
+def stack_dict(inputs, fun=tf.stack):
+    values = []
+    for key in sorted(inputs.keys()):
+      values.append(tf.cast(inputs[key], tf.float32))
+
+    return fun(values, axis=-1)
+
+
 # SETUP
 
 session = f1.get_session(2023, 1, 'R')
@@ -19,7 +27,6 @@ sessions = f1.get_sessions(2023)
 print('----------')
 print('-- F1NN --')
 print('----------')
-print()
 
 #print(session)
 #print(session['results'])
@@ -30,97 +37,122 @@ session_data = f1.get_filtered_session_results(sessions)
 
 # INPUT
 
-print('\n-- INPUT --\n')
+print('\n\n-- INPUT --\n')
 
 numeric_feature_names = ['GridPosition', 'Finished']
 binary_feature_names = []
 categorical_feature_names = ['Abbreviation']
 
-target = session_data.pop('Position')
-
-
-inputs = {}
-for name, column in session_data.items():
-  if type(column.iloc[0]) == str:
-    dtype = tf.string
-  elif (name in categorical_feature_names or
-        name in binary_feature_names):
-    dtype = tf.int64
-  else:
-    dtype = tf.float32
-  
-  inputs[name] = tf.keras.Input(shape=(), name=name, dtype=dtype)
-
-print(inputs)
-
-
-numeric_features = session_data[numeric_feature_names]
-#print(numeric_features)
-print(numeric_features.head())
+#target_names = ['Position']
+targets = session_data.pop('Position')
 
 
 # PREPROCESSING
 
-print('\n-- PREPROCESSING --\n')
+print('\n\n-- PREPROCESSING --\n')
 
-#print(tf.convert_to_tensor(numeric_features))
+inputs = {}
+for name, column in session_data.items():
+    if type(column.iloc[0]) == str:
+        dtype = tf.string
+    elif (name in categorical_feature_names or
+            name in binary_feature_names):
+        dtype = tf.int64
+    else:
+        dtype = tf.float32
+    
+    inputs[name] = tf.keras.Input(shape=(), name=name, dtype=dtype)
+
+print(inputs)
 
 
-# preprocessed = []
+normalizer = tf.keras.layers.Normalization(axis=-1)
 
-# for name in binary_feature_names:
-#   inp = inputs[name]
-#   inp = inp[:, tf.newaxis]
-#   float_value = tf.cast(inp, tf.float32)
-#   preprocessed.append(float_value)
 
-# print(preprocessed)
+preprocessed = []
 
-#print(numeric_features.values)
+# binary features
+for name in binary_feature_names:
+  inp = inputs[name]
+  inp = inp[:, tf.newaxis]
+  float_value = tf.cast(inp, tf.float32)
+  preprocessed.append(float_value)
 
-normalizer = tf.keras.layers.Normalization(axis=None)
-normalizer.adapt(numeric_features.values)
-#normalizer.adapt(dict(numeric_features.values))
-#normalizer.adapt(stack_dict(dict(numeric_features.values)))
+print(preprocessed)
+
+#numeric features
+numeric_inputs = {}
+for name in numeric_feature_names:
+  numeric_inputs[name] = inputs[name]
+
+numeric_inputs = stack_dict(numeric_inputs)
+numeric_normalized = normalizer(numeric_inputs)
+
+preprocessed.append(numeric_normalized)
+
+print(preprocessed)
+
+# categorical features
+for name in categorical_feature_names:
+    vocab = sorted(set(session_data[name]))
+    print(f'name: {name}')
+    print(f'vocab: {vocab}\n')
+
+    if type(vocab[0]) is str:
+        lookup = tf.keras.layers.StringLookup(vocabulary=vocab, output_mode='one_hot')
+    else:
+        lookup = tf.keras.layers.IntegerLookup(vocabulary=vocab, output_mode='one_hot')
+
+    x = inputs[name][:, tf.newaxis]
+    x = lookup(x)
+    preprocessed.append(x)
+
+
+preprocessed_result = tf.concat(preprocessed, axis=-1)
+print(preprocessed_result)
+
+preprocessor = tf.keras.Model(inputs, preprocessed_result)
+
+print(inputs)
 
 
 # MODEL
 
-print('\n-- MODEL --\n')
+print('\n\n-- MODEL --\n')
 
 def get_basic_model():
-  model = tf.keras.Sequential([
-    normalizer,
-    tf.keras.layers.Dense(10, activation='relu'),
-    tf.keras.layers.Dense(10, activation='relu'),
-    tf.keras.layers.Dense(1)
-  ])
+    model = tf.keras.Sequential([
+        normalizer,
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(10, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
 
-  model.compile(optimizer='adam',
-                loss=tf.keras.losses.MeanSquaredError(),
-                metrics=['accuracy'])
-  return model
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.MeanSquaredError(),
+                  metrics=['accuracy'])
+    return model
 
 model = get_basic_model()
 
 
 # TRAINING
 
-print('\n-- TRAINING --\n')
+print('\n\n-- TRAINING --\n')
 
-model.fit(numeric_features, target, epochs=EPOCHS, batch_size=BATCH_SIZE)
+model.fit(inputs, targets, epochs=EPOCHS, batch_size=BATCH_SIZE)
 
 
 # EVALUATION
 
-print('\n-- EVALUATION --\n')
+print('\n\n-- EVALUATION --\n')
 
-model.evaluate(numeric_features, target)
+model.evaluate(inputs, targets)
 
 
 # PREDICTION
 
-print('\n-- PREDICTION --\n')
+print('\n\n-- PREDICTION --\n')
 
-predictions = model.predict(numeric_features)
-print(predictions)
+#predictions = model.predict(inputs.iloc[1])
+#print(predictions)
